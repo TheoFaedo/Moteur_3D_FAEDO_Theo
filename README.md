bbb074ecff1137f141c1751ddc608508b09e5d34
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2024-05-14 20:03:09 +0200

    readme + doc

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Moteur_3D_FAEDO_Theo
+
+## Auteur
+Théo FAEDO
+
+## Présentation du programme
+
+Ce programme est un petit moteur 3D permettant rendre un objet 3D texturés (à partir d'un fichier .obj et d'une texture tga).
+
+Le moteur est conçu pour fonction via le CPU et n'est donc pas adapté pour du temps réel.
+
+### Fonctionnalités
+
+- lecture de fichiers .obj avec leur texture associée
+- éclairage ambiant
+- éclairage speculaire
+- affichage des triangles du modèle
+- texture 
+- utilisation d'un z-buffer
+- caméra perspective
+- choix de l'angle de vue
+
+## Compiler et exécuter
+
+Pour compiler ce programme, vous aurez besoin de cmake (version 3.28 de préférence).
+
+Si vous n'avez pas le projet en local, clonez le projet via git :
+```bash
+>> git clone https://github.com/TheoFaedo/Moteur_3D_FAEDO_Theo.git
+```
+
+Rendez-vous à la racine du projet puis créer le repertoire *build* s'il n'éxiste pas déjà.
+```bash
+>> mkdir build && cd build
+```
+
+Une fois dans le repertoire build, compilez le projet dans celui-ci avec la commande suivante :
+```bash
+>> cmake .. && cmake --build .
+```
+
+Vous pouvez désormais exécuter le programme via la commande :
+```bash
+>> ./moteur3D <chemin_fichier_obj> <chemin_texture> <float_angle>
+```
+
+Un exemple de modèle et de texture est disponible dans le repertoire */obj*.
+```bash
+>> ./moteur3D "../obj/african_head/african_head.obj" "../obj/african_head/african_head_diffuse.tga" 0.5
+```
+Après exécution du programme, un fichier *build/out.tga* contient le résultat du rendu fait par le programme.
+
--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@
 #include <sstream>
 #include <vector>
 
-#define C_SEUIL -0.01
+#define C_SEUIL -0.01 // Permet de corriger les problèmes d'espacements des triangles
 
+/**
+ * @brief Permet de calculer la norme d'un vecteur
+*/
 float norme(std::vector<float> n){
     return std::sqrt(std::pow(n[0], 2) + std::pow(n[1], 2) + std::pow(n[2], 2));
 }
 
 /**
- * @brief Recupere l'index d'un sommet
+ * @brief Recupere l'index d'un sommet d'une composante d'un vecteur de face
+ * @example recv_vertice_index("1/5/4") == 1 and recv_vertice_index("1/5/4", 1) == 5
 */
 int recv_vertice_index(std::string &line, int index = 0){
     int taille = line.size();
@@ -38,14 +42,24 @@ int recv_vertice_index(std::string &line, int index = 0){
     return std::stoi(tmp)-1;
 }
 
+/**
+ * @brief Permet de recuperer la couleur d'un pixel à partir de ses coordonnées barycentriques dans le triangle,
+ * des vecteurs textures du triangle, de la texture et de la normale de la face.
+ * 
+ * @param n Vecteur normal de la face
+ * @param t1, t2, t3 Vecteurs textures
+ * @param alpha, beta, gamma Coordonnées barycentriques
+ * @param textureMap Image de texture
+*/
 TGAColor getColor(std::vector<float> n, std::vector<int> t1, std::vector<int> t2, std::vector<int> t3, float alpha, float beta, float gamma, TGAImage &textureMap){
 
+    // Récupération de l'illumination de la face à partir de la norme
     std::vector<float> lightdir = {0, 0, 1};
     float dot = n[0]*lightdir[0] + n[1]*lightdir[1] + n[2]*lightdir[2];
-    dot = std::max(0.3f, dot);
+    dot = std::max(0.3f, dot); // Lumière ambiante de 0.3
 
+    // Application de la couleur à partir de la texture
     TGAColor color = textureMap.get(t1[0]*alpha + t2[0]*beta + t3[0]*gamma, t1[1]*alpha + t2[1]*beta + t3[1]*gamma);
-    //TGAColor color = {{255, 255, 255, 255}, 4};
 
     return {
         (u_int8_t)floor(color[0] * dot),
@@ -55,6 +69,12 @@ TGAColor getColor(std::vector<float> n, std::vector<int> t1, std::vector<int> t2
     };
 }
 
+/**
+ * @brief Permet de dessiner une ligne
+ * @param x0, y0, x1, y1 : coordonnées des deux extremites
+ * @param image : image dans laquelle on dessine
+ * @param color : couleur de la ligne
+*/
 void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     bool steep = false; 
     if (std::abs(x0-x1)<std::abs(y0-y1)) { 
@@ -85,6 +105,12 @@ void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     } 
 }
 
+/**
+ * @brief Permet de dessiner un triangle
+ * @param x0, y0, x1, y1, x2, y2 : coordonnées des trois extremites
+ * @param image : image dans laquelle on dessine
+ * @param color : couleur du triangle
+*/
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color){
     draw_line(x0, y0, x1, y1, image, color);
     draw_line(x1, y1, x2, y2, image, color);
@@ -93,50 +119,81 @@ void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &ima
 
 /**
  * @brief Draw a filled triangle
+ * @param x0, y0, x1, y1, x2, y2 : coordonnées des trois extremites
+ * @param image : image dans laquelle on dessine
+ * @param t1, t2, t3 : vecteurs textures
+ * @param n1, n2, n3 : vecteurs normaux
+ * @param zbuffer : zbuffer
+ * @param z0, z1, z2 : profondeur des trois extremites
+ * @param textureMap : texture
  */ 
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image,
 std::vector<int> t1, std::vector<int> t2, std::vector<int> t3, TGAImage &textureMap,
 std::vector<float> n1, std::vector<float> n2, std::vector<float> n3, int* zbuffer, 
 float z0, float z1, float z2){
 
+    // On ne dessine que si les points sont dans l'image
     if(x0 > image.width() || y0 > image.height() || x1 > image.width() || y1 > image.height() || x2 > image.width() || y2 > image.height()
     || x0 < 0 || y0 < 0 || x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0)
     return;
 
+    // On calcule les coordonnées des extremites
     std::vector<int> coinGauche = {std::min(x0, std::min(x1, x2)), std::min(y0, std::min(y1, y2))};
     std::vector<int> coinDroit = {std::max(x0, std::max(x1, x2)), std::max(y0, std::max(y1, y2))};
 
+    // Calcul de la matrice inverse (pour le calcul des coéficients barycentriques)
     mat<3,3> ma = {
         vec<3>{(double)x0, (double)x1, (double)x2},
         vec<3>{(double)y0, (double)y1, (double)y2},
         vec<3>{1., 1., 1.}
     };
-
     mat<3,3> matInverse = ma.invert();
 
     #pragma omp parallel for
     for(int y = coinGauche[1]; y < coinDroit[1]; y++){
         for(int x = coinGauche[0]; x <= coinDroit[0]; x++){
+
+            // Calcul des coefficients barycentriques
             float alpha = matInverse[0][0] * x + matInverse[0][1] * y + matInverse[0][2];
             float beta = matInverse[1][0] * x + matInverse[1][1] * y + matInverse[1][2];
             float gamma = matInverse[2][0] * x + matInverse[2][1] * y + matInverse[2][2];
+            
+            // Dessin des points dans triangle
             if((alpha >= C_SEUIL && beta >= C_SEUIL && gamma >= C_SEUIL) || (alpha <= C_SEUIL && beta <= C_SEUIL && gamma <= C_SEUIL) && (alpha != C_SEUIL || beta != C_SEUIL || gamma != C_SEUIL)){
+                
+                // Calcul de la profondeur du point
                 float z = alpha * z0 + beta * z1 + gamma * z2;
+
+                // On ne dessine que si le points est moins profond que le précédement (s'il y en a un)
                 if(zbuffer[x+y*image.width()] < z){
                     zbuffer[x+y*image.width()] = z;
+
+                    // Calcul de la normale de la face
                     std::vector<float> n = {n1[0]*alpha+n2[0]*beta+n3[0]*gamma, n1[1]*alpha+n2[1]*beta+n3[1]*gamma, n1[2]*alpha+n2[2]*beta+n3[2]*gamma};
                     float no = norme(n);
-                    n = {n[0]/no, n[1]/no, n[2]/no};
+                    n = {n[0]/no, n[1]/no, n[2]/no}; // Normalisation
                     
-                    if(n[2] < 0) return;
-                    image.set(x, y, getColor(n, t1, t2, t3, alpha, beta, gamma, textureMap));  
+                    if(n[2] < 0) return; // Si la norme est dans la direction de la profondeur alors on ne dessine pas (peu provoquer des problèmes dans certains cas)
+
+                    // Application de la couleur à l'image  
+                    image.set(x, y, getColor(n, t1, t2, t3, alpha, beta, gamma, textureMap));
                 }  
             }              
         }
     }
 }
 
-int objParser(std::string path, std::vector<std::vector<float>> &sommets, std::vector<std::vector<std::vector<int>>> &faces, std::vector<std::vector<float>> &normales, std::vector<std::vector<int>> &textures, TGAImage &textureMap, int width = 2048, int height = 2048){
+/**
+ * @brief Permet de parser un fichier .obj pour l'utiliser dans le programme
+ * @param path Chemin du fichier .obj
+ * @param sommets, faces, normales, textures : vecteurs qui contiennent les informations de l'objet
+ * @param textureMap : image de texture
+ * @param angle : angle de rotation
+*/
+int objParser(std::string path, 
+std::vector<std::vector<float>> &sommets, std::vector<std::vector<std::vector<int>>> &faces, std::vector<std::vector<float>> &normales, 
+std::vector<std::vector<int>> &textures, 
+TGAImage &textureMap, double angle = 0.4){
     std::ifstream file(path);
 
     if (file.is_open()) {
@@ -145,30 +202,31 @@ int objParser(std::string path, std::vector<std::vector<float>> &sommets, std::v
             std::istringstream iss(line);
             std::string label;
 
-            iss >> label;
-
-            double angle = 0.5;
+            iss >> label; // lecture du label (v, vn, f, vt)
 
             // Parsing
-            if (label == "v") {
+            if (label == "v") { // sommets
                 float x, y, z;
                 iss >> x >> y >> z;
-                //sommets.push_back({(int)(x*(width/2)+width/2), (int)(y*(height/2)+height/2), (int)(z*height)});
                 sommets.push_back({(float)(x*cos(angle) - z*sin(angle)), y, (float)(x*sin(angle) + z*cos(angle))});
-            } else if (label == "f"){
+
+            } else if (label == "f"){ // faces
                 std::string v1, v2, v3;
                 iss >> v1 >> v2 >> v3;
                 faces.push_back({{recv_vertice_index(v1), recv_vertice_index(v1, 1)}, {recv_vertice_index(v2), recv_vertice_index(v2, 1)}, {recv_vertice_index(v3), recv_vertice_index(v3, 1)}});
-            } else if (label == "vn"){
+
+            } else if (label == "vn"){ // normales
                 float x, y, z;
                 iss >> x >> y >> z;
                 normales.push_back({(float)(x*cos(angle) - z*sin(angle)), y, (float)(x*sin(angle) + z*cos(angle))});
-            } else if (label == "vt"){
+
+            } else if (label == "vt"){ // textures
                 float x, y, z;
                 iss >> x >> y >> z;
                 textures.push_back({(int)floor(x*textureMap.width()), textureMap.height()-((int)floor(y*textureMap.height())), 0});
+            
             } else {
-                if(label != "#" && label != "") std::cerr << "Error parsing line: " << line << std::endl;
+                if(label != "#" && label != "" && label != "g" && label != "s") std::cerr << "Error parsing line: " << line << std::endl;
             }
         }
         file.close();
@@ -180,10 +238,18 @@ int objParser(std::string path, std::vector<std::vector<float>> &sommets, std::v
     return 0;
 }
 
+/**
+ * @brief Permet de transformer par la perspective un point de coordonnées orthographiques
+ * @param sommet : point de coordonnées orthographiques
+ * @param tanAby2 : tangente de l'angle du champs de vision divisé par deux
+ * @param width, height : largeur et hauteur de l'image
+*/
 std::vector<int> to_perspective(std::vector<float> sommet, double tanAby2, int width, int height){
 
-    float newX = sommet[0]/(((sommet[2]-3))*tanAby2);
-    float newY = -sommet[1]/(((sommet[2]-3))*tanAby2);
+    float cameraZ = 3;
+
+    float newX = sommet[0]/(((sommet[2]-cameraZ))*tanAby2);
+    float newY = -sommet[1]/(((sommet[2]-cameraZ))*tanAby2);
 
     return {
         (int)(newX*(width/2)+width/2),
@@ -193,68 +259,63 @@ std::vector<int> to_perspective(std::vector<float> sommet, double tanAby2, int w
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    int model = 0;
 
-    constexpr int width = 1024;
-    constexpr int height = 1024;
-    TGAColor white = {{255, 255, 255, 255}, 4};
-    TGAColor red = {{0, 0, 255, 255}, 4};
-
+    float angle = 0.0;
     std::string obj_path;
     std::string texture_path;
 
-    if(model == 0){
-        obj_path = "../obj/african_head/african_head.obj";
-        texture_path = "../obj/african_head/african_head_diffuse.tga";
-    }else if(model == 1){
-        obj_path = "../obj/boggie/body.obj";
-        texture_path = "../obj/boggie/body_diffuse.tga";
-    }else{
-        obj_path = "../obj/diablo3_pose/diablo3_pose.obj";
-        texture_path = "../obj/diablo3_pose/diablo3_pose_diffuse.tga";
+    if(argc >= 3 && argc <= 4){
+        obj_path = argv[1];
+        texture_path = argv[2];
+        if(argc == 4) angle = std::stof(argv[3]);
+    } else {
+        std::cerr << "Usage: " << argv[0] << " <obj_path> <texture_path> <angle>" << std::endl;
+        return 1;
     }
+
+    constexpr int width = 2048;
+    constexpr int height = 2048;
     
-    //Initialisation de l'image
+    // Initialisation de l'image
     TGAImage image(width, height, TGAImage::RGB);
 
-    //Initialisation du zbuffer
+    // Initialisation du zbuffer
     int* zbuffer = (int*)malloc(width*height*sizeof(int));
     for (int i = 0; i < width * height; i++) {
         zbuffer[i] = -100000;
     }
 
-    //Loading texture map
+    // Loading texture map
     TGAImage textureMap = TGAImage();
     textureMap.read_tga_file(texture_path);
 
-    //Création des listes de sommets, normales, textures et faces
+    // Création des listes de sommets, normales, textures et faces
     std::vector<std::vector<float>> sommetsRelative;
     std::vector<std::vector<int>> sommets;
     std::vector<std::vector<std::vector<int>>> faces;
     std::vector<std::vector<float>> normales;
-    std::vector<std::vector<int>> textures;
+    std::vector<std::vector<int>> textures; 
     
-    //Parsing du fichier obj
-    objParser(obj_path, sommetsRelative, faces, normales, textures, textureMap, width, height);
+    // Parsing du fichier obj
+    objParser(obj_path, sommetsRelative, faces, normales, textures, textureMap, angle);
 
-    //Création de la valeur de FOV
+    // Création de la valeur de FOV
     double fov = M_PI/4.;
     double tanAby2 = tan(fov/2);
 
-    //Transformation en vecteurs perspective
+    // Transformation en vecteurs perspective
     for(int i = 0; i < sommetsRelative.size(); i++){
         sommets.push_back(to_perspective(sommetsRelative[i], tanAby2, width, height));
     } 
 
-    //Dessin du maillage sur le plan de l'écran
-    std::cout << faces.size() << std::endl;
+    // Dessin des triangles sur l'image
     for(int i = 0; i<faces.size(); i++){
         draw_filled_triangle(
             sommets[faces[i][0][0]][0], sommets[faces[i][0][0]][1], 
             sommets[faces[i][1][0]][0], sommets[faces[i][1][0]][1], 
-            sommets[faces[i][2][0]][0], sommets[faces[i][2][0]][1], 
+            sommets[faces[i][2][0]][0], sommets[faces[i][2][0]][1],
             image, 
             textures[faces[i][0][1]], textures[faces[i][1][1]], textures[faces[i][2][1]], textureMap,
             normales[faces[i][0][0]], normales[faces[i][1][0]], normales[faces[i][2][0]], 
@@ -264,6 +325,6 @@ int main()
     }
 
     //Sauvegarde de l'image
-    image.write_tga_file("test_out.tga", true, false);
+    image.write_tga_file("out.tga", true, false);
     return 0;
 }
